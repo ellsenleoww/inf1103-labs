2b2ae2b15a4f832d635cea615668ff2f7437cbf1
ellsen <ellsenleow1@gmail.com>
2026-09-04 17:14:09 +0800

    Second activity

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("===========")
 print("Welcome here")
 print("Hello")
 print("===========")
+
+username = "ellsen"
+bio = "Fun Blogger"
+followers = 50
+
+print("Username:", username)
+print("Bio:", bio)
+print("Follower", followers)
