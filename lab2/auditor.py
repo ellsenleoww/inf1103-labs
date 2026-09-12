a21580dbc5c598bccd18c36848736078dba8082e
ellsenleoww <2602173@sit.singaporetech.edu.sg>
2026-09-12 19:07:41 +0800

    part 3 & part 4

--- lab2/auditor.py
+++ lab2/auditor.py
@@ -1,8 +1,10 @@
 inventory = 0
-failed_entries = 0
 
 while True:
     stock_quantity = input("Enter stock quantity: ")
 
     if stock_quantity.lower() == "quit":
         break
+    
+    elif not stock_quantity.isdigit():
+        print("Enter an integer.")
