78a05312306305e4fc9a1a4a80a357fa53cea0b4
ellsenleoww <2602173@sit.singaporetech.edu.sg>
2026-09-19 15:53:05 +0800

    get_valid_input() function

--- lab2/modular_auditor.py
+++ lab2/modular_auditor.py
@@ -1,1 +1,21 @@
-inventory = 0
+failed_entries = 0
+
+def get_valid_input():
+    global failed_entries
+
+    while True:
+        stock_quantity = input("Enter stock quantity: ")
+
+        if stock_quantity.lower() == "quit":
+            return "quit"
+
+        elif stock_quantity[:1] == "-" and stock_quantity[1:].isdigit():
+            print("Negative numbers not accepted.")
+            failed_entries += 1
+
+        elif not stock_quantity.isdigit():
+            print("Enter an integer.")
+            failed_entries += 1
+            
+        else:
+            return int(stock_quantity)
